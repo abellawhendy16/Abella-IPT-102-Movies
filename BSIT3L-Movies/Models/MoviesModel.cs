b5f7666a3e3177ef7ac720abfefa06f64e8d453b
abellawhendy16 <abella.whendy.06162002@gmail.com>
2023-10-14 16:00:21 +0800

    add imagepath and videopath

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 namespace BSIT3L_Movies.Models
 {
-    
-	public class MovieViewModel
+
+    public class MovieViewModel
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+        public string ImagePath { get; set; }
+        public string VideoPath { get; set; }
+
     }
 }
-
